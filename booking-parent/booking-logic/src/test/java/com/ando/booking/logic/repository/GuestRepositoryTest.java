8679da7c946514259061679dfd0482ca29f131b7
andolsi <andolsi@andolsi-Vostro-3350>
2014-06-06 00:32:15 +0200

    Neo4jRule added

--- booking-parent/booking-logic/src/test/java/com/ando/booking/logic/repository/GuestRepositoryTest.java
+++ booking-parent/booking-logic/src/test/java/com/ando/booking/logic/repository/GuestRepositoryTest.java
@@ -1,30 +1,47 @@
 package com.ando.booking.logic.repository;
-
+import static com.lordofthejars.nosqlunit.neo4j.Neo4jRule.Neo4jRuleBuilder.newNeo4jRule;
 import static org.junit.Assert.assertNotNull;
 
+import java.util.Iterator;
+
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.data.neo4j.conversion.EndResult;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.ando.booking.logic.domain.entities.Guest;
+import com.lordofthejars.nosqlunit.annotation.UsingDataSet;
+import com.lordofthejars.nosqlunit.core.LoadStrategyEnum;
+import com.lordofthejars.nosqlunit.neo4j.Neo4jRule;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:spring/application-context-logic-all-test.xml"})
 public class GuestRepositoryTest {
+	
+	@Autowired
+	private ApplicationContext applicationContext;
+	
 	@Autowired
 	private GuestRepository guestRepository ;
 	
+	@Rule
+    public Neo4jRule neo4jRule = newNeo4jRule().defaultSpringGraphDatabaseServiceNeo4j();
+	
 	@Test
+	@UsingDataSet(locations = "/com/ando/booking/logic/repository/repository-dataset.xml", loadStrategy = LoadStrategyEnum.CLEAN_INSERT)
 	public void saveGuest(){
-		Guest guest = new Guest();
-		guest.setFirstName("Zied");
-		guest.setLastName("Andolsi");
-		assertNotNull(guestRepository);
-		guestRepository.save(guest);
-		guest=guestRepository.findOne(guest.getId());
-		assertNotNull(guest);
+		EndResult<Guest> guests=guestRepository.findAll();
+		assertNotNull(guests);
+		Iterator<Guest> itartor= guests.iterator();
+		while(itartor.hasNext()){
+			Guest guest=itartor.next();
+			System.out.println(guest.getId());
+			System.out.println(guest.getFirstName());
+		}
 	}
 
 }
