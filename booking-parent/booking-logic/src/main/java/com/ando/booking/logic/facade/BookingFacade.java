6c0e06933198be60d8bbb290c2806c0a1eafce9a
AndolsiZied <andolsi.zied@yahoo.fr>
2014-06-06 18:37:56 +0200

    findAllGuests method added

--- booking-parent/booking-logic/src/main/java/com/ando/booking/logic/facade/BookingFacade.java
+++ booking-parent/booking-logic/src/main/java/com/ando/booking/logic/facade/BookingFacade.java
@@ -1,8 +1,13 @@
 package com.ando.booking.logic.facade;
 
+import java.util.List;
+
 import com.ando.booking.logic.domain.entities.Guest;
 
 public interface BookingFacade {
+
 	Guest save(Guest guest);
+
+	List<Guest> findAllGuests();
 
 }
