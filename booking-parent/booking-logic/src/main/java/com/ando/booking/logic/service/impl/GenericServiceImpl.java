1ad3964d5d12e9e607b1281da68d00ea82bd3699
AndolsiZied <andolsi.zied@yahoo.fr>
2014-06-06 18:38:55 +0200

    findAll method added

--- booking-parent/booking-logic/src/main/java/com/ando/booking/logic/service/impl/GenericServiceImpl.java
+++ booking-parent/booking-logic/src/main/java/com/ando/booking/logic/service/impl/GenericServiceImpl.java
@@ -1,8 +1,11 @@
 package com.ando.booking.logic.service.impl;
 
+import java.util.List;
+
 import org.springframework.data.neo4j.repository.GraphRepository;
 
 import com.ando.booking.logic.service.GenericService;
+import com.google.common.collect.Lists;
 
 public abstract class GenericServiceImpl<Type> implements GenericService<Type> {
 
@@ -14,6 +17,10 @@ public abstract class GenericServiceImpl<Type> implements GenericService<Type> {
 
 	public Type findOne(Long id) {
 		return repository.findOne(id);
+	}
+
+	public List<Type> findAll() {
+		return Lists.newArrayList(repository.findAll());
 	}
 
 }
