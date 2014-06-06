67a12050c1106a6fc2435d33b6cf82f673cdbef2
AndolsiZied <andolsi.zied@yahoo.fr>
2014-06-06 18:38:30 +0200

    findAll method added

--- booking-parent/booking-logic/src/main/java/com/ando/booking/logic/service/GenericService.java
+++ booking-parent/booking-logic/src/main/java/com/ando/booking/logic/service/GenericService.java
@@ -1,10 +1,13 @@
 package com.ando.booking.logic.service;
 
+import java.util.List;
 
 public interface GenericService<Type> {
 
 	Type save(Type entity);
 
 	Type findOne(Long id);
+
+	List<Type> findAll();
 
 }
--- booking-parent/booking-logic/src/main/java/com/ando/booking/logic/service/GuestService.java
+++ booking-parent/booking-logic/src/main/java/com/ando/booking/logic/service/GuestService.java
@@ -3,4 +3,5 @@ package com.ando.booking.logic.service;
 import com.ando.booking.logic.domain.entities.Guest;
 
 public interface GuestService extends GenericService<Guest> {
+
 }
