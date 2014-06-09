0997857cc9a8c29cd809da644b760ac184c1958e
andolsi <andolsi@andolsi-Vostro-3350>
2014-06-09 16:13:34 +0200

    code review

--- booking-parent/booking-logic/src/main/java/com/ando/booking/logic/facade/impl/BookingFacadeImpl.java
+++ booking-parent/booking-logic/src/main/java/com/ando/booking/logic/facade/impl/BookingFacadeImpl.java
@@ -2,29 +2,61 @@ package com.ando.booking.logic.facade.impl;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.ando.booking.logic.domain.entities.Guest;
 import com.ando.booking.logic.facade.BookingFacade;
 import com.ando.booking.logic.service.GuestService;
+import com.ando.booking.logic.util.exception.BookingLogicException;
+import com.ando.booking.logic.util.exception.DataBaseException;
 
+/**
+ * This class implements the Booking's logic tier facade.
+ * 
+ * @author Zied ANDOLSI
+ */
 @Service
 @Transactional
-public class BookingFacadeImpl implements BookingFacade {
+public class BookingFacadeImpl extends AbstractFacade implements BookingFacade {
 
-	@Autowired
-	private GuestService guestService;
+	private static final Logger LOGGER = LoggerFactory.getLogger(BookingFacadeImpl.class);
 
-	@Override
-	public Guest save(Guest guest) {
-		return guestService.save(guest);
+	public BookingFacadeImpl() {
+
 	}
 
-	@Override
-	public List<Guest> findAllGuests() {
-		return guestService.findAll();
+	public BookingFacadeImpl(GuestService guestService) {
+		this.guestService = guestService;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public Guest createGuest(Guest guest) throws BookingLogicException {
+		LOGGER.info("starting createGuest method...");
+		try {
+			return guestService.save(guest);
+		} catch (DataBaseException e) {
+			LOGGER.error("error occured when trying to create guest.", e);
+			throw new BookingLogicException(e);
+		}
+
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public List<Guest> getAllGuests() throws BookingLogicException {
+		LOGGER.info("starting getAllGuests method...");
+		try {
+			return guestService.findAll();
+		} catch (DataBaseException e) {
+			LOGGER.error("error occured when trying to load all guests.", e);
+			throw new BookingLogicException(e);
+		}
 	}
 
 }
