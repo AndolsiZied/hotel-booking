09fcc3efdfe4ec9ab11e09a9164bb8a860d82ece
andolsi <andolsi@andolsi-Vostro-3350>
2014-06-09 16:15:18 +0200

    code review

--- booking-parent/booking-logic/src/test/java/com/ando/booking/logic/repository/GuestRepositoryTest.java
+++ booking-parent/booking-logic/src/test/java/com/ando/booking/logic/repository/GuestRepositoryTest.java
@@ -1,12 +1,14 @@
 package com.ando.booking.logic.repository;
+
 import static com.lordofthejars.nosqlunit.neo4j.Neo4jRule.Neo4jRuleBuilder.newNeo4jRule;
 import static org.junit.Assert.assertNotNull;
-
-import java.util.Iterator;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.data.neo4j.conversion.EndResult;
@@ -14,34 +16,39 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.ando.booking.logic.domain.entities.Guest;
+import com.google.common.collect.Lists;
 import com.lordofthejars.nosqlunit.annotation.UsingDataSet;
 import com.lordofthejars.nosqlunit.core.LoadStrategyEnum;
 import com.lordofthejars.nosqlunit.neo4j.Neo4jRule;
 
+/**
+ * This class {@link GuestRepositoryTest} tests behavior of the {@link GuestRepository} methods.
+ * 
+ * @author Zied ANDOLSi
+ */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"classpath:spring/application-context-logic-all-test.xml"})
+@ContextConfiguration(locations = { "classpath:spring/application-context-logic-all-test.xml" })
 public class GuestRepositoryTest {
-	
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(GuestRepositoryTest.class);
+
 	@Autowired
 	private ApplicationContext applicationContext;
-	
+
 	@Autowired
-	private GuestRepository guestRepository ;
-	
+	private GuestRepository guestRepository;
+
 	@Rule
-    public Neo4jRule neo4jRule = newNeo4jRule().defaultSpringGraphDatabaseServiceNeo4j();
-	
+	public Neo4jRule neo4jRule = newNeo4jRule().defaultSpringGraphDatabaseServiceNeo4j();
+
 	@Test
 	@UsingDataSet(locations = "/com/ando/booking/logic/repository/repository-dataset.xml", loadStrategy = LoadStrategyEnum.CLEAN_INSERT)
-	public void saveGuest(){
-		EndResult<Guest> guests=guestRepository.findAll();
+	public void findAll() {
+		LOGGER.debug("starting test findAll method...");
+		EndResult<Guest> guests = guestRepository.findAll();
 		assertNotNull(guests);
-		Iterator<Guest> itartor= guests.iterator();
-		while(itartor.hasNext()){
-			Guest guest=itartor.next();
-			System.out.println(guest.getId());
-			System.out.println(guest.getFirstName());
-		}
+		assertTrue("size of returned list must be 2", Lists.newArrayList(guests.iterator()).size() == 2);
+		LOGGER.debug("testing saveGuest method ends.");
 	}
 
 }
