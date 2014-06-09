7f8861d8109c7f3a3339a17577a38a80485919b2
andolsi <andolsi@andolsi-Vostro-3350>
2014-06-09 16:18:19 +0200

    code review

--- booking-parent/booking-presentation/src/main/java/com/ando/booking/presentation/controller/GuestController.java
+++ booking-parent/booking-presentation/src/main/java/com/ando/booking/presentation/controller/GuestController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.ando.booking.logic.facade.BookingFacade;
+import com.ando.booking.logic.util.exception.BookingLogicException;
 
 @Controller
 public class GuestController {
@@ -22,7 +23,11 @@ public class GuestController {
 	public ModelAndView getListUsersView() {
 		LOGGER.debug("Received request to get guest list view");
 		ModelMap model = new ModelMap();
-		model.addAttribute("users", bookingFacade.findAllGuests());
+		try {
+			model.addAttribute("users", bookingFacade.getAllGuests());
+		} catch (BookingLogicException e) {
+			LOGGER.error("error occurred when trying to load all guests : " + e.getLocalizedMessage());
+		}
 		return new ModelAndView("guest_list", model);
 	}
 }
