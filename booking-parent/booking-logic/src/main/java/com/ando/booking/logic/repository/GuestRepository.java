4533a92764baeef6d5f3d7462effc42aea4dddc9
andolsi <andolsi@andolsi-Vostro-3350>
2014-06-09 16:13:57 +0200

    comments added

--- booking-parent/booking-logic/src/main/java/com/ando/booking/logic/repository/GuestRepository.java
+++ booking-parent/booking-logic/src/main/java/com/ando/booking/logic/repository/GuestRepository.java
@@ -4,6 +4,11 @@ import org.springframework.data.neo4j.repository.GraphRepository;
 
 import com.ando.booking.logic.domain.entities.Guest;
 
+/**
+ * This interface defines methods required to manage guest entity in database.
+ * 
+ * @author Zied ANDOLSI
+ */
 public interface GuestRepository extends GraphRepository<Guest> {
 
 }
