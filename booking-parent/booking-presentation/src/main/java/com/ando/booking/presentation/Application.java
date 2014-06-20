e4cd15220a028c75a0c9559df1d3d8f17c41fa77
AndolsiZied <andolsi.zied@yahoo.fr>
2014-06-20 17:24:16 +0200

    configuration updated

--- booking-parent/booking-presentation/src/main/java/com/ando/booking/presentation/Application.java
+++ booking-parent/booking-presentation/src/main/java/com/ando/booking/presentation/Application.java
@@ -2,19 +2,25 @@ package com.ando.booking.presentation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportResource;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.Import;
+
+import com.ando.booking.logic.LogicConfiguration;
 
 @Configuration
-@PropertySource("classpath:application.properties")
 @ComponentScan
 @EnableAutoConfiguration
-@ImportResource(value = "application-context-presentation-all.xml")
-public class Application {
+@Import(value = LogicConfiguration.class)
+public class Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	protected final SpringApplicationBuilder configure(final SpringApplicationBuilder application) {
+		return application.sources(Application.class);
 	}
 }
